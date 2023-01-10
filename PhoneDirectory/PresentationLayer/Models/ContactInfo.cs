d727dbf95f60d768a3a04dbfafee1b6153f07344
harunkarakaya <karakayaharun67@gmail.com>
2023-01-11 01:22:37 +0300

    Create model class

---
+++ PhoneDirectory/PresentationLayer/Models/ContactInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace PresentationLayer.Models
+{
+    public class ContactInfo
+    {
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Location { get; set; }
+        public string InformationContent { get; set; }
+    }
+}
